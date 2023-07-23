fe47502ffa5512f81482bc855f87a5e367186688
datpdgcd18369 <datpdgcd18369@fpt.edu.vn>
2023-07-23 16:51:35 +0700

    Push Ref
    Learn how use Refs and the DOM

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useRef } from 'react';
+ 
 function App() {
+  const inputRef =useRef(null)
+  function onAddToCard(text){
+    alert(text)
+  }
+  const way2 =(text)=>{
+    alert(text)
+  }
+  const way3 =()=>{
+    alert(inputRef.current.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='panel'>
+        <div className='panel-heading'>Add User</div>
+        <div className='panel-body'>
+            <div className='form-group'>
+              <input className='form-control' type='text' name id placeholder='Info User'  ref={inputRef}/>
+            </div>
+            <button className='btn btn-primary' onClick={()=>onAddToCard(inputRef.current.value)}>gữi 01 </button>
+            <button className='btn btn-primary 'onClick={()=>way2(inputRef.current.value)}>gữi 02 </button>
+            <button className='btn btn-primary'onClick={way3}>gữi 03 </button>
+        </div>
+      </div>
     </div>
   );
 }
